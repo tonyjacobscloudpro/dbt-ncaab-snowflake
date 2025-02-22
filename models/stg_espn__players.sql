7090e49b99d67c601cb312806ffdf4f17b849c3d
TONY JACOBS <tjacobs.datascience@gmail.com>
2025-02-22 10:08:01 +0000

    dims and fact

--- models/stg_espn__players.sql
+++ models/stg_espn__players.sql
@@ -1,11 +1,10 @@
 select
-    seq4() AS id,
+    seq4() AS playerid,
     SPLIT_PART(player.player, ' ', 1) as first_name,
     SPLIT_PART(player.player, ' ', 2) as last_name,
-    player.team,
-    team.teamid,
-    team.team_name,
-    player.jersey
+    player,
+    team,
+    jersey
 from (
     select distinct player, team, jersey
     from RAW.ESPN.NCAAM_DAILY_STATS
